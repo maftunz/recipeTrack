984d3d073b9f303d9e491d329f2b00b6c8fd85cb
Maftuna Zoirjonova <maftuna_zoirjonova@student.itpu.uz>
2025-05-19 18:10:22 +0500

    update

--- recipe_track/settings.py
+++ recipe_track/settings.py
@@ -123,5 +123,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-TELEGRAM_BOT_TOKEN = '7638540551:AAEM4QhLlZIrOmrQjCapmmdPrAsdP3mCrYo'
